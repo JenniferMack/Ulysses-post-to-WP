c6eba3ce46f9152f50c82441e4284f84ab6f68a3
Jennifer Mack <jenn@jennifermack.net>
2016-05-19 14:53:31 -0700

    Update to use password file

--- wp-post.rb
+++ wp-post.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby -E UTF-8:UTF-8
 
 require 'xmlrpc/client'
+require 'json'
+
+begin
+  pwfile = File.expand_path("~/.ulysses-wp-password.json")
+  pwinfo = JSON.parse(File.open(pwfile).read)
+rescue => err
+  puts "Password error: #{err}"
+  exit
+end
+
 
 wordpress = XMLRPC::Client.new_from_hash(
-  { "host" => "yourblogurl.com",
-    "user" => "yourWPusername",
-    "password" => "yourWPpassword",
-    "use_ssl" => nil,   #change to true if hosted on SSL
-#-------------------------------------------------------
-    "path" => "/xmlrpc.php"
+  { "host"     => pwinfo["host"],
+    "user"     => pwinfo["user"],
+    "password" => pwinfo["password"],
+    "use_ssl"  => pwinfo["use_ssl"],
+    "path"     => "/xmlrpc.php"
   })
 
 post = []
@@ -22,10 +31,6 @@ end
 title = post.shift.gsub(/<\/*h1>/, '')
 tags  = post.shift.gsub(/<\/*p>/, '').split(/,\s*/)
 
-# Edit to change delay in hours:
-delay = 2
-post_time = Time.now.utc + (60 * 60 * delay)
-
 content["post_title"]   = title
 
 # Comment this line to use categories instead of tags
@@ -35,11 +40,10 @@ content["terms_names"]  = {"post_tag" => tags}
 #content["terms_names"]  = {"category" => tags}
 
 content["post_content"] = post.join("\n\n").sub(/<p>MORE<\/p>/, '<!--more-->')
-#content["comment_status"] = "open"
+content["comment_status"] = "open"
 
-# Post time must be in UTC
-content["post_status"]  = "future"
-content["post_date_gmt"] = post_time.strftime("%F %R")
+# Set draft post status
+content["post_status"]  = "draft"
 
 begin
   postnum = wordpress.call(
